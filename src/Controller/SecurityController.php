3076c1f060603888bbf888bf17ba709d21e57535
Marco8301 <marcanmalas@gmail.com>
2021-07-15 15:10:17 +0200

    🔧 [Apache] - Added apache-pack

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Form\LoginFormType;
 use App\Service\SecurityService;
-use Firebase\JWT\JWT;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
