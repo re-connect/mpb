91fc044f7f046ec55d538b05b3d9be0f8202554b
Marco8301 <marcanmalas@gmail.com>
2021-07-19 15:00:20 +0200

    🗃 [BugReport Entity] - Added new properties

--- src/Entity/BugReport.php
+++ src/Entity/BugReport.php
@@ -12,6 +12,23 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class BugReport
 {
+    const APPLICATIONS = [
+        0 => 'Coffre-fort Numérique',
+        1 => 'Reconnect Pro',
+        2 => 'Application Mobile',
+    ];
+    const ENVIRONMENTS = [
+        0 => 'Pre-production',
+        1 => 'Production'
+    ];
+    const ACCOUNT_TYPE = [
+        0 => 'CFN - Bénéficiaire',
+        1 => 'CFN - Membre',
+        2 => 'CFN - Gestionnaire',
+        3 => 'CFN - Admin',
+        4 => 'RP - Membre',
+        5 => 'RP - Admin',
+    ];
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
@@ -57,6 +74,77 @@ class BugReport
      * @ORM\JoinColumn(nullable=false)
      */
     private $status;
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $application;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $device;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $device_language;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $device_os;
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $device_os_version;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $browser;
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $browser_version;
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $history;
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $environment;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $url;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $account_id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $account_type;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $item_id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $other_info;
+
+    static public function getConstValues($array): array
+    {
+        $output = [];
+        foreach ($array as $key => $value) {
+            $output[$value] = $key;
+        }
+
+        return $output;
+    }
 
     public function __construct()
     {
@@ -209,6 +297,174 @@ class BugReport
     public function setStatus(?Status $status): self
     {
         $this->status = $status;
+
+        return $this;
+    }
+
+    public function getApplication(): ?int
+    {
+        return $this->application;
+    }
+
+    public function setApplication(int $application): self
+    {
+        $this->application = $application;
+
+        return $this;
+    }
+
+    public function getDevice(): ?string
+    {
+        return $this->device;
+    }
+
+    public function setDevice(string $device): self
+    {
+        $this->device = $device;
+
+        return $this;
+    }
+
+    public function getDeviceLanguage(): ?string
+    {
+        return $this->device_language;
+    }
+
+    public function setDeviceLanguage(string $device_language): self
+    {
+        $this->device_language = $device_language;
+
+        return $this;
+    }
+
+    public function getDeviceOs(): ?string
+    {
+        return $this->device_os;
+    }
+
+    public function setDeviceOs(string $device_os): self
+    {
+        $this->device_os = $device_os;
+
+        return $this;
+    }
+
+    public function getDeviceOsVersion(): ?string
+    {
+        return $this->device_os_version;
+    }
+
+    public function setDeviceOsVersion(?string $device_os_version): self
+    {
+        $this->device_os_version = $device_os_version;
+
+        return $this;
+    }
+
+    public function getBrowser(): ?string
+    {
+        return $this->browser;
+    }
+
+    public function setBrowser(string $browser): self
+    {
+        $this->browser = $browser;
+
+        return $this;
+    }
+
+    public function getBrowserVersion(): ?string
+    {
+        return $this->browser_version;
+    }
+
+    public function setBrowserVersion(?string $browser_version): self
+    {
+        $this->browser_version = $browser_version;
+
+        return $this;
+    }
+
+    public function getHistory(): ?string
+    {
+        return $this->history;
+    }
+
+    public function setHistory(string $history): self
+    {
+        $this->history = $history;
+
+        return $this;
+    }
+
+    public function getEnvironment(): ?int
+    {
+        return $this->environment;
+    }
+
+    public function setEnvironment(int $environment): self
+    {
+        $this->environment = $environment;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(?string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getAccountId(): ?int
+    {
+        return $this->account_id;
+    }
+
+    public function setAccountId(int $account_id): self
+    {
+        $this->account_id = $account_id;
+
+        return $this;
+    }
+
+    public function getAccountType(): ?int
+    {
+        return $this->account_type;
+    }
+
+    public function setAccountType(int $account_type): self
+    {
+        $this->account_type = $account_type;
+
+        return $this;
+    }
+
+    public function getItemId(): ?int
+    {
+        return $this->item_id;
+    }
+
+    public function setItemId(?int $item_id): self
+    {
+        $this->item_id = $item_id;
+
+        return $this;
+    }
+
+    public function getOtherInfo(): ?string
+    {
+        return $this->other_info;
+    }
+
+    public function setOtherInfo(?string $other_info): self
+    {
+        $this->other_info = $other_info;
 
         return $this;
     }
--- src/Form/BugReportType.php
+++ src/Form/BugReportType.php
@@ -7,6 +7,9 @@ use App\Entity\Category;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -28,8 +31,76 @@ class BugReportType extends AbstractType
             ->add('title', TextType::class, [
                 'label' => 'Titre du Bug Report'
             ])
+            ->add('application', ChoiceType::class, [
+                'label' => 'Application concernée par le bug',
+                'choices' => BugReport::getConstValues(BugReport::APPLICATIONS)
+            ])
+            ->add('device', TextType::class, [
+                'label' => 'Appareil',
+                'attr' => [
+                    'placeholder' => 'Ordinateur, smartphone, tablette...'
+                ]
+            ])
+            ->add('deviceLanguage', TextType::class, [
+                'label' => 'Langue de l\'appareil',
+                'attr' => [
+                    'placeholder' => 'Quelle est la langue configurée sur l\'appareil ?'
+                ]
+            ])
+            ->add('deviceOs', TextType::class, [
+                'label' => 'Système d\'exploitation de l\'appareil',
+                'attr' => [
+                    'placeholder' => 'Windows, MacOS, Android, iOS...'
+                ]
+            ])
+            ->add('deviceOsVersion', TextType::class, [
+                'label' => 'Version du système d\'exploitation de l\'appareil',
+                'required' => false
+            ])
+            ->add('browser', TextType::class, [
+                'label' => 'Navigateur internet utilisé',
+                'attr' => [
+                    'placeholder' => 'Chrome, Firefox, Internet Explorer, Safari, Navigateur du téléphone...'
+                ]
+            ])
+            ->add('browserVersion', TextType::class, [
+                'label' => 'Version du navigateur internet utilisé',
+                'required' => false
+            ])
             ->add('content', CKEditorType::class, [
                 'label' => 'Description du bug rencontré',
+            ])
+            ->add('history', CKEditorType::class, [
+                'label' => 'Historique des actions éxécutées menant au bug (1 ligne = 1 action)',
+            ])
+            ->add('environment', ChoiceType::class, [
+                'label' => 'Environnement sur lequel a lieu le bug',
+                'choices' => BugReport::getConstValues(BugReport::ENVIRONMENTS)
+            ])
+            ->add('url', TextType::class, [
+                'label' => 'URL sur laquelle a lieu le bug',
+                'attr' => [
+                    'placeholder' => 'exemple : https://pro.reconnect.fr/families'
+                ],
+                'required' => false
+            ])
+            ->add('accountId', IntegerType::class, [
+                'label' => 'ID du compte concerné par le bug'
+            ])
+            ->add('accountType', ChoiceType::class, [
+                'label' => 'Type de profil utilisé lors du bug',
+                'choices' => BugReport::getConstValues(BugReport::ACCOUNT_TYPE)
+            ])
+            ->add('itemId', IntegerType::class, [
+                'label' => 'Si pertinent, ID de l\'item problématique',
+                'attr' => [
+                    'placeholder' => 'exemple : id du bénéficiaire, id du ménage, id du document...'
+                ],
+                'required' => false
+            ])
+            ->add('otherInfo', TextType::class, [
+                'label' => 'Autre information pouvant aider à la reproduction du bug',
+                'required' => false,
             ])
             ->add('category', EntityType::class, [
                 'label' => 'Je considère ce bug comme...',
@@ -45,7 +116,7 @@ class BugReportType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => BugReport::class,
-        ]);
+           'data_class' => BugReport::class,
+       ]);
     }
 }
