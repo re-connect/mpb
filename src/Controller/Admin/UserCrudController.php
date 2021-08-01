89d74c2cffd306bec5f1d566645b26b740459494
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2021-08-01 16:22:30 +0200

    ✨ [Admin] Add dropdown for roles

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -12,14 +15,14 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('email'),
+            TextField::new('firstName'),
+            TextField::new('lastName'),
+            DateTimeField::new('lastLogin'),
+            ChoiceField::new('role')->setChoices(array_combine(User::ROLES,User::ROLES)),
         ];
     }
-    */
 }
