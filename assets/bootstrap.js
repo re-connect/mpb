ad7e4ec859e39787f3e72d752dc0d7821f1eca27
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2022-07-08 15:52:32 +0200

    ⬆️ [Recipes] Upgrade

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -3,9 +3,9 @@ import Reveal from "stimulus-reveal-controller";
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 const app = startStimulusApp(require.context(
-    '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
-    true,
-    /\.(j|t)sx?$/
+  '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
+  true,
+  /\.[jt]sx?$/
 ));
 
 // register any custom, 3rd party controllers here
