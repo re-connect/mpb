0ae20bfde74200aed41c5668c622b596608ff78a
AntoineAresu <you@example.com>
2022-09-23 18:09:42 +0200

    :bug: [Deployer] Fix deploy

--- deploy.php
+++ deploy.php
@@ -23,17 +23,20 @@ add('writable_dirs', []);
 
 host('155.133.130.39')
     ->set('remote_user', 'www-data')
-    ->set('deploy_path', '~/mpb-test/www');
+    ->set('deploy_path', '~/mpb');
 
 // Tasks
 
 task('deploy:build_frontend', function () {
-    run('npm install && npm run build');
+    run('cd {{release_path}} && yarn install && yarn build');
 });
-
+task('deploy:assets_install', function () {
+    run('cd {{release_path}} && php bin/console ckeditor:install && php bin/console assets:install public');
+});
 // Hooks
 
-before('deploy:cache:clear', 'deploy:build_frontend');
+before('deploy:assets_install', 'deploy:build_frontend');
+before('deploy:cache:clear', 'deploy:assets_install');
 before('deploy:symlink', 'database:migrate');
 
 after('deploy:failed', 'deploy:unlock');
