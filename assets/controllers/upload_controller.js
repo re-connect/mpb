a661ca6f16a2e768677f764ceff27f0bacdf656c
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2022-09-25 14:25:07 +0200

    :arrow_up: [Uppy] Upgrade major from 2.* to 3.*

--- assets/controllers/upload_controller.js
+++ assets/controllers/upload_controller.js
@@ -1,7 +1,15 @@
-import { Controller } from '@hotwired/stimulus';
-import * as Uppy from "uppy";
+import {Controller} from '@hotwired/stimulus';
+import Uppy from '@uppy/core'
 import axios from "axios";
 import fr from "@uppy/locales/lib/fr_FR";
+import XHRUpload from '@uppy/xhr-upload'
+import Dashboard from '@uppy/dashboard'
+import Webcam from '@uppy/webcam'
+
+// And their styles (for UI plugins)
+// With webpack and `style-loader`, you can import them like this:
+import '@uppy/core/dist/style.css'
+import '@uppy/dashboard/dist/style.css'
 
 /*
 * The following line makes this controller "lazy": it won't be downloaded until needed
@@ -9,37 +17,37 @@ import fr from "@uppy/locales/lib/fr_FR";
 */
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
-  static targets = [
-    'container'
-  ]
-  static values = {
-    url: String,
-    callbackUrl: String,
-  }
+    static targets = [
+        'container'
+    ]
+    static values = {
+        url: String,
+        callbackUrl: String,
+    }
 
-  connect() {
-    const uppy = new Uppy.Core({ locale: fr })
-      .use(Uppy.Webcam, {})
-      .use(Uppy.Dashboard, {
-        trigger: this.containerTarget,
-        target: 'body',
-        plugins: ['Webcam'],
-        allowMultipleUploads: false,
-        closeModalOnClickOutside: true,
-        closeAfterFinish: true,
-        proudlyDisplayPoweredByUppy: false,
-      })
-      .use(Uppy.XHRUpload, {
-        endpoint: this.urlValue,
-        formData: true,
-        fieldName: 'file',
-      })
-      .on('complete', () => {
-        if (this.callbackUrlValue) {
-          axios.get(this.callbackUrlValue).then((response) => {
-            this.containerTarget.parentNode.innerHTML = response.data;
-          });
-        }
-      });
-  }
+    connect() {
+        new Uppy({locale: fr})
+            .use(Webcam, {})
+            .use(Dashboard, {
+                trigger: this.containerTarget,
+                target: 'body',
+                plugins: ['Webcam'],
+                allowMultipleUploads: false,
+                closeModalOnClickOutside: true,
+                closeAfterFinish: true,
+                proudlyDisplayPoweredByUppy: false,
+            })
+            .use(XHRUpload, {
+                endpoint: this.urlValue,
+                formData: true,
+                fieldName: 'file',
+            })
+            .on('complete', () => {
+                if (this.callbackUrlValue) {
+                    axios.get(this.callbackUrlValue).then((response) => {
+                        this.containerTarget.parentNode.innerHTML = response.data;
+                    });
+                }
+            });
+    }
 }
