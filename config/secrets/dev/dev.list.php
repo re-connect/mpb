47e4fad77f4c77a26c65563229c4a5e4caf8b11b
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2022-10-15 11:08:04 +0200

    :wrench: [Env files] Improve vault usage

--- config/secrets/dev/dev.list.php
+++ config/secrets/dev/dev.list.php
@@ -1,5 +1,10 @@
 <?php
 
 return [
+    'MAILER_DSN' => null,
+    'OAUTH_GOOGLE_CLIENT_ID' => null,
+    'OAUTH_GOOGLE_CLIENT_SECRET' => null,
+    'RECONNECT_PRO_CLIENT_ID' => null,
+    'RECONNECT_PRO_CLIENT_SECRET' => null,
     'SLACK_DSN' => null,
 ];
