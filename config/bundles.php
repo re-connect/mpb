97d19678c54bcc1b26cefacf4470bb2e58235de4
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2022-10-31 08:52:47 +0100

    :heavy_plus_sign: [Turbo] Install

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
