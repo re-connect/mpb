fdf611c80e98d4a24211778fcd872307efb66364
AntoineAresu <aresu.antoine@gmail.com>
2023-04-18 17:05:34 +0200

    :sparkles: [AttachmentController] Refacto bug routes, add feature routes

--- src/Controller/AttachmentController.php
+++ src/Controller/AttachmentController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Bug;
 use App\Entity\UserRequest;
+use App\Entity\Feature;
 use App\Security\Voter\Permissions;
 use App\Service\AttachmentService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,18 +16,36 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[Route('/attachments')]
 class AttachmentController extends AbstractController
 {
-    #[Route(path: '/bug/{id}', name: 'add_attachment', methods: ['POST'])]
+    #[Route(path: '/bug/{id}', name: 'bug_add_attachment', methods: ['POST'])]
     #[IsGranted(Permissions::READ, 'bug')]
     public function addBugAttachment(Request $request, Bug $bug, AttachmentService $service): Response
     {
         return $this->addAttachment($bug, $request->files->get('file'), $service);
     }
 
-    #[Route(path: '/bug/{id}/widget', name: 'attachment_widget', methods: ['GET'])]
+    #[Route(path: '/bug/{id}/widget', name: 'bug_attachment_widget', methods: ['GET'])]
     #[IsGranted(Permissions::READ, 'bug')]
     public function getBugAttachmentWidget(Bug $bug): Response
     {
-        return $this->render('attachment/_widget.html.twig', ['userRequest' => $bug]);
+        return $this->render('bug/_attachment_widget.html.twig', [
+            'bug' => $bug,
+        ]);
+    }
+
+    #[Route(path: '/feature/{id}', name: 'feature_add_attachment', methods: ['POST'])]
+    #[IsGranted(Permissions::READ, 'feature')]
+    public function addFeatureAttachment(Request $request, Feature $feature, AttachmentService $service): Response
+    {
+        return $this->addAttachment($feature, $request->files->get('file'), $service);
+    }
+
+    #[Route(path: '/feature/{id}/widget', name: 'feature_attachment_widget', methods: ['GET'])]
+    #[IsGranted(Permissions::READ, 'feature')]
+    public function getFeatureAttachmentWidget(Feature $feature): Response
+    {
+        return $this->render('feature/components/_attachment_widget.html.twig', [
+            'feature' => $feature,
+        ]);
     }
 
     private function addAttachment(UserRequest $userRequest, mixed $file, AttachmentService $service): Response
