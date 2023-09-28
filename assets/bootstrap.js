f61293feae5a4ecdb7c9b47d2211a43429f9978e
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2023-09-28 10:07:18 +0200

    :arrow_up: [Composer] Upgrade webpack encore

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,8 +1,8 @@
-import { startStimulusApp } from '@symfony/stimulus-bridge';
 import Reveal from "stimulus-reveal-controller";
+import { startStimulusApp } from '@symfony/stimulus-bridge';
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
-const app = startStimulusApp(require.context(
+export const app = startStimulusApp(require.context(
   '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
   true,
   /\.[jt]sx?$/
@@ -11,5 +11,3 @@ const app = startStimulusApp(require.context(
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
 app.register("reveal", Reveal);
-
-export { app };
