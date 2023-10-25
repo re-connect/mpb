2e1defb527b230e9d796a1fc2d4b300e2f7ec463
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2023-10-25 15:42:22 +0200

    :arrow_up: [Frontend] Fix frontend deps

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,5 @@
-import './bootstrap.js';
 import '@popperjs/core';
-import 'tom-select/dist/scss/tom-select.scss';
-import './styles/app.scss';
+import './bootstrap.js';
 import enableTooltips from './scripts/turbo_helper';
 
 global.$ = global.jQuery = require('jquery');
