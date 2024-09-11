3d114f3dd8d2aea78848beb946b802c13529f85b
AntoineAresu <aresu.antoine@gmail.com>
2024-09-11 15:16:31 +0200

    :rocket: [Deploy] Setup script for preprod

--- deploy.php
+++ deploy.php
@@ -7,29 +7,44 @@ require 'recipe/symfony.php';
 // Config
 
 set('repository', 'git@github.com:re-connect/mpb.git');
-set('branch', 'main');
+set('remote_user', 'www-data');
 set('flush_cache_file_name', 'flush-cache.php');
 set('flush_cache_file_path', '{{current_path}}/public/{{flush_cache_file_name}}');
+set('env', [
+    'SYMFONY_ENV' => get('symfony_env'),
+]);
 
 add('shared_files', [
     '.env',
     '.env.local',
     'config/secrets/prod/prod.decrypt.private.php',
+    'config/secrets/prod/preprod.decrypt.private.php',
 ]);
 add('shared_dirs', [
     'var/log',
     'var/oauth',
-    'node_modules',
-    'vendor',
+    'node_modules/',
+    'vendor/',
     'public/uploads',
 ]);
 
 add('writable_dirs', []);
 
 // Hosts
+host('preprod')
+    ->setHostname('155.133.130.39')
+    ->setLabels(['stage' => 'preprod'])
+    ->set('branch', 'dev')
+    ->set('symfony_env', 'preprod')
+    ->set('homepage_url', 'https://preprod.mpb.reconnect.fr')
+    ->set('deploy_path', '~/mpb_pp');
 
-host('155.133.130.39')
-    ->set('remote_user', 'www-data')
+host('prod')
+    ->setHostname('155.133.130.39')
+    ->setLabels(['stage' => 'prod'])
+    ->set('branch', 'main')
+    ->set('symfony_env', 'prod')
+    ->set('homepage_url', 'https://mpb.reconnect.fr')
     ->set('deploy_path', '~/mpb');
 
 // Tasks
@@ -53,8 +68,9 @@ task('deploy:reset-opcache', function () {
 });
 // Hooks
 
-before('deploy:install_frontend', 'deploy:assets_install');
 before('deploy:build_frontend', 'deploy:install_frontend');
+before('deploy:install_frontend', 'deploy:assets_install');
+before('deploy:cache:clear', 'deploy:build_frontend');
 before('deploy:symlink', 'deploy:reset-opcache');
 after('deploy:reset-opcache', 'database:migrate');
 
